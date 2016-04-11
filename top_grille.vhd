3976753285a3e8552312a265529e4b8f8efca8cf
simrobs <robard.simon@gmail.com>
2016-04-11 18:05:51 +0200

    Added files via upload

---
+++ top_grille.vhd
@@ -0,0 +1,115 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:23:07 03/25/2016 
+-- Design Name: 
+-- Module Name:    top_grille - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_grille is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+			  select_case_in : in STD_LOGIC_VECTOR (6 downto 0);
+           vga_hs : out  STD_LOGIC;
+           vga_vs : out  STD_LOGIC;
+			  select_case_out : in STD_LOGIC_VECTOR (6 downto 0);
+           vga_red : out  STD_LOGIC_VECTOR (3 downto 0);
+           vga_green : out  STD_LOGIC_VECTOR (3 downto 0);
+           vga_blue : out  STD_LOGIC_VECTOR (3 downto 0);
+           data_out : out  STD_LOGIC_VECTOR (2 downto 0));
+			  
+end top_grille;
+
+architecture Behavioral of top_grille is
+
+
+component VGA_bitmap_160x100 is
+
+  generic(bit_per_pixel : integer range 1 to 12 := 3;    -- number of bits per pixel
+          grayscale     : boolean := false);           -- should data be displayed in grayscale
+  port(clk          : in  std_logic;
+       reset        : in  std_logic;
+       VGA_hs       : out std_logic;   -- horisontal vga syncr.
+       VGA_vs       : out std_logic;   -- vertical vga syncr.
+       VGA_red      : out std_logic_vector(3 downto 0);   -- red output
+       VGA_green    : out std_logic_vector(3 downto 0);   -- green output
+       VGA_blue     : out std_logic_vector(3 downto 0);   -- blue output
+
+       ADDR         : in  std_logic_vector(13 downto 0);
+       data_in      : in  std_logic_vector(bit_per_pixel - 1 downto 0);
+       data_write   : in  std_logic;
+       data_out     : out std_logic_vector(bit_per_pixel - 1 downto 0));
+		 
+end component VGA_bitmap_160x100;
+
+
+
+component vga_grille is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+			  select_case : in STD_LOGIC_VECTOR (6 downto 0);
+           ADDR : out  STD_LOGIC_VECTOR (13 downto 0);
+           data_in : out  STD_LOGIC_VECTOR (2 downto 0);
+           data_write : out  STD_LOGIC);
+end component vga_grille;
+
+
+signal sig_data_in : STD_LOGIC_VECTOR (2 downto 0);
+signal sig_data_write : STD_LOGIC;
+signal sig_ADDR : STD_LOGIC_VECTOR (13 downto 0);
+
+
+
+begin
+
+
+
+gestion_affichage : VGA_bitmap_160x100 port map (  clk 			=> clk,       
+																	reset  		=> rst,    
+																	VGA_hs   	=> vga_hs,  
+																	VGA_vs   	=> vga_vs,  
+																	VGA_red  	=> vga_red,  
+																	VGA_green	=> vga_green,  
+																	VGA_blue 	=> vga_blue,  
+																	
+																	ADDR       	=> sig_ADDR, 
+																	data_in    	=> sig_data_in,
+																	data_write 	=> sig_data_write,
+																	data_out   	=> data_out);
+														
+
+affichage_grille : vga_grille port map (  			clk 			=> clk,       
+																	rst  		   => rst,   
+																	select_case => select_case_out,
+																	ADDR       	=> sig_ADDR, 
+																	data_in    	=> sig_data_in,
+																	data_write 	=> sig_data_write);
+																														
+														
+														
+
+
+
+
+end Behavioral;
+
---
+++ vga_grille.vhd
@@ -0,0 +1,231 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    12:36:03 03/25/2016 
+-- Design Name: 
+-- Module Name:    vga_grille - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity vga_grille is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+			  select_case : in STD_LOGIC_VECTOR (6 downto 0);
+           ADDR : out  STD_LOGIC_VECTOR (13 downto 0);
+           data_in : out  STD_LOGIC_VECTOR (2 downto 0);
+           data_write : out  STD_LOGIC);
+			  
+end vga_grille;
+
+architecture Behavioral of vga_grille is
+
+signal sig_ADDR : integer range 0 to 15999 := 0;
+signal cnt_enable : std_logic;
+
+begin
+
+process(clk, rst)	
+
+	begin
+		if (rst = '1') then				-- Init
+			data_in <= "000";
+			data_write <= '0';
+			sig_ADDR <= 0;
+			
+		elsif rising_edge(clk) then
+		if cnt_enable = '1' then
+			if sig_ADDR >= 159999 then -- Compteur parcourant tous les pixels
+				sig_ADDR <= 0;
+			else
+				sig_ADDR <= sig_ADDR + 1;
+				
+			end if;
+		end if;
+		end if;
+	end process;
+		
+-----------------Trace la grille------------
+
+process(clk,rst)
+begin
+		if (rst = '1') then				-- Init
+			data_in <= "000";
+			data_write <= '0';
+			sig_ADDR <= 0;
+			cnt_enable <= '0';
+			
+		elsif rising_edge(clk) then
+			
+			if ( (sig_ADDR >= 1470 AND sig_ADDR <= 1570) OR (sig_ADDR >= 3070 AND sig_ADDR <= 3170) OR (sig_ADDR >= 4670 AND sig_ADDR <= 4770) OR (sig_ADDR >= 6270 AND sig_ADDR <= 6370)OR(sig_ADDR >= 7870 AND sig_ADDR <=7970)OR (sig_ADDR >= 9470 AND sig_ADDR <= 9570) OR (sig_ADDR >= 11070 AND sig_ADDR <= 11170) OR (sig_ADDR >= 12670 AND sig_ADDR <= 12770) OR  (sig_ADDR >= 14270 AND sig_ADDR <= 14370)  ) then
+				ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));-- trace les lignes
+				data_in <= "001"; -- bleu
+				data_write <= '1';
+				cnt_enable <= '1';
+				
+			elsif( (sig_ADDR >= 30 AND sig_ADDR <= 15869) AND (((sig_ADDR MOD 160) = 30) OR ((sig_ADDR MOD 160) = 40) OR ((sig_ADDR MOD 160) = 50) OR ((sig_ADDR MOD 160) = 60) OR ((sig_ADDR MOD 160) = 70) OR ((sig_ADDR MOD 160) = 80) OR ((sig_ADDR MOD 160) = 90) OR ((sig_ADDR MOD 160) = 100) OR ((sig_ADDR MOD 160) = 110) OR ((sig_ADDR MOD 160) = 120) OR ((sig_ADDR MOD 160) = 130))) then 
+				ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));--trace les colonnes
+				data_in <= "001"; -- bleu
+				data_write <= '1';
+				cnt_enable <= '1';
+			
+			end if;
+			
+----------------------------------------------
+
+--		
+----------Trace la case en surbrillance----------
+else
+				case select_case is -- Gestion position barre de selection
+					when "0000000" => -- case 0
+						if (sig_ADDR >= 1311 AND sig_ADDR <= 1319) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; --jaune
+							data_write <= '1';
+							cnt_enable <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000";--noir
+							data_write <= '1';
+							cnt_enable <= '1';
+						end if;
+					when "0000001" => -- case 1
+						if (sig_ADDR >= 1321 AND sig_ADDR <= 1329) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; --jaune
+							data_write <= '1';
+							cnt_enable <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; --noir
+							data_write <= '1';
+							cnt_enable <= '1';
+						end if;
+					when "0000010" => -- case 2
+						if (sig_ADDR >= 1331 AND sig_ADDR <= 1339) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+							cnt_enable <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+							cnt_enable <= '1';
+						end if;
+					when "0000011" => -- case 3
+						if (sig_ADDR >= 1341 AND sig_ADDR <= 1349) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+						end if;
+					when "0000100" => -- case 4
+						if (sig_ADDR >= 1351 AND sig_ADDR <= 1359) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+						end if;
+					when "0000101" => -- case 5
+						if (sig_ADDR >= 1361 AND sig_ADDR <= 1369) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+						end if;
+					when "0000110" => -- case 6
+						if (sig_ADDR >= 1371 AND sig_ADDR <= 1379) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+						end if;
+					when "0000111" => -- case 7
+						if (sig_ADDR >= 1381 AND sig_ADDR <= 1389) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; -- noir
+							data_write <= '1';
+						end if;
+					when "0001000" => -- case 8
+						if (sig_ADDR >= 1391 AND sig_ADDR <= 1399) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; --noir
+							data_write <= '1';
+							end if;
+							
+					when "0001001" => -- case 9
+						if (sig_ADDR >= 1401 AND sig_ADDR <= 1409) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; --noir
+							data_write <= '1';
+							end if;
+							
+					when "0001010" => -- case 10
+						if (sig_ADDR >= 2911 AND sig_ADDR <= 2919) then
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "110"; -- jaune
+							data_write <= '1';
+						else
+							ADDR <= std_logic_vector(to_unsigned(sig_ADDR,14));
+							data_in <= "000"; --noir
+							data_write <= '1';
+						end if;
+					when others => data_write <= '0';
+				end case;
+
+
+
+-----------------------------------------------
+			
+		end if;
+		
+	
+	
+end process;
+
+		
+end Behavioral;
+
